d41b7324e894dad6c9ee9f362c9da8389d763719
Ali <ali.youssef.2896@gmail.com>
2025-11-27 20:59:14 +0200

    Main menu stuff

--- Scripts/UI/main_menu.gd
+++ Scripts/UI/main_menu.gd
@@ -9,6 +9,10 @@ func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/City/CityPlayable.tscn")
 
 
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
+
+
 func _on_mute_button_toggled(toggled_on: bool) -> void:
 	if toggled_on:
 		AudioServer.set_bus_mute(bus, true)
