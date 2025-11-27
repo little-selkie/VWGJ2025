74a746be81e766af8d2711e6bce0c46c0bf9421a
Rin <rin.kramarenko@gmail.com>
2025-11-27 19:12:58 +0200

    images to all events added

--- Scripts/UI/win_lose_pop_up.gd
+++ Scripts/UI/win_lose_pop_up.gd
@@ -8,14 +8,16 @@ func _on_exit_button_pressed() -> void:
 func _on_draw() -> void:
 	if !GlobalVars.lose:
 		$Sounds/Win.play()
-		$BGPanel/Panel/VBoxContainer/Header.text = "You Win!"
-		$BGPanel/Panel/VBoxContainer/Context.text = "You managed to fully protect your city. No more destruction and pain, your people may finally rest and get back to their normal lives. \r\n You may continue in Endless Mode with no air strike threats."
+		$BGPanel/Panel/VBoxContainer/HeaderText.text = "You Win!"
+		$BGPanel/Panel/VBoxContainer/HBoxContainer/DescriptionText.text = "You managed to fully protect your city. No more destruction and pain, your people may finally rest and get back to their normal lives. \r\n You may continue in Endless Mode with no air strike threats."
 		$BGPanel/Panel/VBoxContainer/Buttons/FirstButton.text = "Endless Mode"
+		$BGPanel/Panel/VBoxContainer/HBoxContainer/EventImage.texture = load("res://Icons/Funny/win.jpg")
 	elif GlobalVars.lose:
 		$Sounds/Lose.play()
-		$BGPanel/Panel/VBoxContainer/Header.text = "You Lose."
-		$BGPanel/Panel/VBoxContainer/Context.text = "You have no money and no means to protect your city. \r\n Do you wish to try again?"
+		$BGPanel/Panel/VBoxContainer/HeaderText.text = "You Lose."
+		$BGPanel/Panel/VBoxContainer/HBoxContainer/DescriptionText.text = "You have no money and no means to protect your city. \r\n Do you wish to try again?"
 		$BGPanel/Panel/VBoxContainer/Buttons/FirstButton.text = "Try Again"
+		$BGPanel/Panel/VBoxContainer/HBoxContainer/EventImage.texture = load("res://Icons/Funny/lose.jpg")
 	get_tree().paused = true
 
 
